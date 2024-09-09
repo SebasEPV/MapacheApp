049961105c04a85ce48860e8f2a5c9c1051f2855
unknown <sperzvinas@gmail.com>
2024-09-09 17:05:48 -0500

    Added Whatsapp button

--- app/view/layouts/nav.php
+++ app/view/layouts/nav.php
@@ -42,6 +42,10 @@
             navLinks.classList.toggle('show');
         }
     </script>
+
+    <a href="https://wa.me/+529984123305" target="_blank" id="whatsapp-button" title="WhatsApp">
+        <img src="./../../../public/assets/imgs/Logo-WhatsApp.png" alt="WhatsApp Logo" style="width: 50px; height: 50px;" />
+    </a>
 </body>
 
 </html>
