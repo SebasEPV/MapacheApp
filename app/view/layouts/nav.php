7faeeddedfcf1668051d5e543fdb8db5a724fdb8
unknown <sperzvinas@gmail.com>
2024-09-09 17:13:01 -0500

    Added Whatsapp button

--- app/view/layouts/nav.php
+++ app/view/layouts/nav.php
@@ -45,6 +45,7 @@
 
     <a href="https://wa.me/+529984123305" target="_blank" id="whatsapp-button" title="WhatsApp">
         <img src="./../../../public/assets/imgs/Logo-WhatsApp.png" alt="WhatsApp Logo" style="width: 50px; height: 50px;" />
+        <span class="whatsapp-text">Hablanos de tu proyecto!</span>
     </a>
 </body>
 
